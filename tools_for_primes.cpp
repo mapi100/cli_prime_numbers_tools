80f77950e016b26624511c2ee09145743bcf62e7
Marco Antono Peña Ibarra <MarcoAntonio270204@gmail.com>
2025-08-05 23:02:09 -0500

    Update tools_for_primes.cpp

--- tools_for_primes.cpp
+++ tools_for_primes.cpp
@@ -2,111 +2,107 @@
 #include <vector>
 #include <cmath>
 
-// A function is used to check if the number given is a prime number
-bool isPrime(long long int number) {
-    if (number <= 1) {
-        return false;
-    }
-    // Check divisibility up to the square root of the number
-    for (long long int divisor = 2; divisor <= std::sqrt(number); divisor++) {
-        if (number % divisor == 0) {
-            return false;
-        }
+using namespace std;
+
+// Funcion que determina si un numero es primo
+bool esPrimo(long long int numero) {
+    if (numero <= 1) return false;
+
+    for (long long int divisor = 2; divisor <= sqrt(numero); ++divisor) {
+        if (numero % divisor == 0) return false;
     }
     return true;
 }
 
-// A function is used to print all prime numbers up to a given limit
-// Basically, is the Sieve of Eratosthenes algorithm
-void printPrimesUpToLimit(int limit) {
-    std::vector<bool> isPrime(limit + 1, true);
+// Imprime todos los numeros primos hasta un limite dado usando la Criba de Eratostenes
+void imprimirPrimosHasta(int limite) {
+    vector<bool> esPrimo(limite + 1, true);
 
-    for (int p = 2; p * p <= limit; p++) {
-        if (isPrime[p]) {
-	    // Mark multiples of p as non-prime
-            for (int i = p * p; i <= limit; i += p) {
-                isPrime[i] = false;
+    esPrimo[0] = esPrimo[1] = false;
+
+    for (int p = 2; p * p <= limite; ++p) {
+        if (esPrimo[p]) {
+            for (int i = p * p; i <= limite; i += p) {
+                esPrimo[i] = false;
             }
         }
     }
 
-    std::cout << "Prime numbers up to " << limit << " are:\n";
-    for (int p = 2; p <= limit; p++) {
-        if (isPrime[p]) {
-                // Prime numbers are printed
-		std::cout << p << " ";
-        }
+    cout << "Numeros primos hasta " << limite << ":\n";
+    for (int p = 2; p <= limite; ++p) {
+        if (esPrimo[p]) cout << p << " ";
     }
-    std::cout << "\n";
+    cout << "\n";
 }
 
-// A function is used to find the closest prime number to a given input
-void findClosestPrime(long long int number) {
-    if (isPrime(number)) {
-        std::cout << "The number itself, " << number << ", is a prime number.\n";
+// Encuentra el numero primo mas cercano a un numero dado
+void encontrarPrimoCercano(long long int numero) {
+    if (esPrimo(numero)) {
+        cout << "El numero " << numero << " ya es primo.\n";
         return;
     }
 
-    long long int smallerPrime = number - 1;
-    long long int largerPrime = number + 1;
+    long long int menor = numero - 1;
+    long long int mayor = numero + 1;
 
-    // To find the closest smaller prime of the given input
-    while (!isPrime(smallerPrime)) {
-        smallerPrime--;
-    }
+    while (!esPrimo(menor)) --menor;
+    while (!esPrimo(mayor)) ++mayor;
 
-    while (!isPrime(largerPrime)) {
-        largerPrime++;
-    }
+    cout << "El primo mas cercano a " << numero << " es: ";
+    if (numero - menor <= mayor - numero)
+        cout << menor;
+    else
+        cout << mayor;
 
-    // Same as above, but with a larger prime
-    std::cout << "Closest prime number to " << number << " is: ";
-    if (number - smallerPrime <= largerPrime - number) {
-        std::cout << smallerPrime;
-    } else {
-        std::cout << largerPrime;
-    }
-    std::cout << "\n";
+    cout << "\n";
 }
 
-// A function to find all twin primes up to a given limit
-void findTwinPrimesUpToLimit(int limit) {
-    std::cout << "Twin primes up to " << limit << " are:\n";
-    for (int p = 2; p <= limit - 2; p++) {
-        if (isPrime(p) && isPrime(p + 2)) {
-            std::cout << "(" << p << ", " << p + 2 << ") ";
+// Encuentra todos los primos gemelos hasta un cierto limite
+void encontrarPrimosGemelosHasta(int limite) {
+    cout << "Primos gemelos hasta " << limite << ":\n";
+    for (int p = 2; p <= limite - 2; ++p) {
+        if (esPrimo(p) && esPrimo(p + 2)) {
+            cout << "(" << p << ", " << p + 2 << ") ";
         }
     }
-    std::cout << "\n";
+    cout << "\n";
 }
 
-// Choose options are provided in order to let the user choose the mode
+// Funcion principal con menu interactivo
 int main() {
-    int option;
-    std::cout << "Choose an option:\n";
-    std::cout << "1. Print all prime numbers up to a limit.\n";
-    std::cout << "2. Find the closest prime number to a given input.\n";
-    std::cout << "3. Find all twin primes up to a limit.\n";
-    std::cout << "Option: ";
-    std::cin >> option;
+    int opcion;
 
-    if (option == 1) {
-        int limit;
-        std::cout << "Enter the limit: ";
-        std::cin >> limit;
-        printPrimesUpToLimit(limit);
-    } else if (option == 2) {
-        long long int number;
-        std::cout << "Enter a number: ";
-        std::cin >> number;
-        findClosestPrime(number);
-    } else if (option == 3) {
-        int limit;
-        std::cout << "Enter the limit: ";
-        std::cin >> limit;
-        findTwinPrimesUpToLimit(limit);
-    } else {
-        std::cout << "Invalid option!\n";
+    cout << "Elige una opcion:\n";
+    cout << "1. Imprimir todos los primos hasta un limite.\n";
+    cout << "2. Encontrar el primo mas cercano a un numero.\n";
+    cout << "3. Encontrar todos los primos gemelos hasta un limite.\n";
+    cout << "Opcion: ";
+    cin >> opcion;
+
+    switch (opcion) {
+        case 1: {
+            int limite;
+            cout << "Ingresa el limite: ";
+            cin >> limite;
+            imprimirPrimosHasta(limite);
+            break;
+        }
+        case 2: {
+            long long int numero;
+            cout << "Ingresa un numero: ";
+            cin >> numero;
+            encontrarPrimoCercano(numero);
+            break;
+        }
+        case 3: {
+            int limite;
+            cout << "Ingresa el limite: ";
+            cin >> limite;
+            encontrarPrimosGemelosHasta(limite);
+            break;
+        }
+        default:
+            cout << "Opcion invalida!\n";
     }
 
     return 0;
